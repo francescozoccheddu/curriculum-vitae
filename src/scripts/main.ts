14918f7624b535dd3319c309c011ba90f2540b45
Francesco Zoccheddu <23437496+francescozoccheddu@users.noreply.github.com>
2024-09-26 09:41:48 +0200

    Update update date

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,8 +1,6 @@
 import { setupPager } from './utils/pager';
 import { setupSkillsWords } from './utils/skillsWords';
-import { setupToday } from './utils/today';
 
-setupToday();
 setupPager();
 void document.fonts.ready.then(() => {
   setupSkillsWords();
